3bb91d33e76850a033578da41294f1d542f71739
Oscar De Alva <oscar.ivan98@gmail.com>
2023-12-09 17:09:30 -0600

    Added formating methods to Multimedia enum

--- app/supported_media.py
+++ app/supported_media.py
@@ -6,6 +6,13 @@ class Multimedia(Enum):
     ANIME = 3
     MOVIE = 4
     VIDEOGAME = 5
+    def msg_format(self) -> str:
+        return self.name.replace('_', ' ').capitalize()
+    
+    @classmethod
+    def categories(cls) -> str:
+        return ', '.join([i.msg_format() for i in Multimedia])
+
     @classmethod
     def validate_option(cls, response: str) -> bool:
         return response.isdigit() and int(response) in [i.value for i in Multimedia] 
@@ -14,3 +21,4 @@ class Multimedia(Enum):
     def formatted(cls) -> str:
         fmt = [f"[{m.value}] {m.name.replace('_', ' ')}S" for m in Multimedia]
         return '\n'.join(fmt)
+
