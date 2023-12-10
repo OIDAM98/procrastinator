dae30270eb02f4add31957f2b91a58326a92ddbe
Oscar De Alva <oscar.ivan98@gmail.com>
2023-12-09 19:28:29 -0600

    Methods to generate Summary of session

--- app/modules/parser_module.py
+++ app/modules/parser_module.py
@@ -1,3 +1,5 @@
+import re
+from app.supported_media import Multimedia
 class Parser:
     @staticmethod
     def get_user_input(msg: str, max=3, validate_function=None) -> list[str]:
@@ -32,3 +34,39 @@ class Parser:
     @staticmethod
     def validate_saving_option(opt: str) -> bool:
         return opt.lower() in ('yes', 'no', 'n', 'y')
+
+    @staticmethod
+    def clean_recommendations(recommendations: list[str], choices: list[Multimedia]) -> list[str]:
+        if len(choices) == 0:
+            return Parser._clean_general_recommendations(recommendations)
+
+        return Parser._clean_specific_recommendations(recommendations, choices)
+    
+    @staticmethod
+    def _clean_specific_recommendations(recommendations: list[str], choices: list[Multimedia]) -> list[str]:
+        regex = r"\d\."
+        clean_recoms = []
+        zipped = zip(choices, recommendations)
+        for (cat, r) in zipped:
+            lines = r.split('\n\n')
+            only_recoms = filter(lambda x: re.match(regex, x), lines)
+            remove_numbers = map(lambda l: l[3:], only_recoms)
+            clean_recoms.append(f"{cat.msg_format()}:")
+            for c in remove_numbers:
+                clean_recoms.append(f"- {c}")
+        
+        return clean_recoms
+    
+    @staticmethod
+    def _clean_general_recommendations(recommendations: list[str]) -> list[str]:
+        clean_recoms = []
+        for r in recommendations:
+            lines = r.split('\n\n')[1:-1]
+            items = list(map(lambda l: l.split('\n'), lines))
+            for [cat, *recoms] in items:
+                remove_numbers = map(lambda l: l[3:], recoms)
+                clean_recoms.append(cat)
+                for c in remove_numbers:
+                    clean_recoms.append(f"- {c}")
+        
+        return clean_recoms
